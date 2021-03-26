c9cd3a4b4b63feadd7cb0503bcee1da8026a1527
Tobias Geiger <tobias.geiger@gmx.at>
2021-03-26 16:20:40 +0100

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Clientside javascript file loaded in!")
-
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 const messageOne = document.querySelector("#message-1")
