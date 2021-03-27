0ed9025591bde8955c06a0347fa8455710264374
Tobias Geiger <tobias.geiger@gmx.at>
2021-03-27 22:49:06 +0100

    Updated forecast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location", undefined)
 
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees!")
+                callback(undefined, "Localtime: " + body.location.localtime + " " + body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out there. It feels like " + body.current.feelslike + " degrees!")
         }
     })
 }
